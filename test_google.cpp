52e9514c7149bb55f84c53c22ca2c0a56713b7c4
dmitriy.loginov <dmitriy.loginov@1520signal.ru>
2023-05-22 00:20:17 +0300

    Added google tests

---
+++ test_google.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "lib.h"
+
+
+TEST(test_version, test_valid_version)
+{
+	EXPECT_TRUE(buildNumber() > 0);
+}
+
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
