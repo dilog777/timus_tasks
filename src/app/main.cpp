975a88336b1af5b43d6bef181c0e9045dc024224
dmitriy.loginov <dmitriy.loginov@1520signal.ru>
2023-06-04 21:36:59 +0300

    Added MyVector

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,21 +1,71 @@
+#include <iostream>
 #include <map>
 #include <vector>
 
 #include "MyAllocator.hpp"
+#include "MyVector.hpp"
+
+
+template<template<typename...> typename Map, typename... Subtypes>
+void fillMapByFactorial(Map<Subtypes...> &map, int size)
+{
+	int factorial = 1;
+	map[0] = factorial;
+
+	for (int i = 1; i < size; ++i)
+	{
+		factorial = factorial * i;
+		map[i] = factorial;
+	}
+}
+
+
+
+template<template<typename...> typename Map, typename... Subtypes>
+void printMap(Map<Subtypes...> &map)
+{
+	for (const auto &pair : map)
+		std::cout << pair.first << " " << pair.second << std::endl;
+}
+
+
+
+template<template<typename...> typename Vector, typename... Subtypes>
+void fillVectorByRange(Vector<Subtypes...> &vector, int size)
+{
+	for (int i = 0; i < size; ++i)
+		vector.push_back(i);
+}
+
+
+
+template<template<typename...> typename Vector, typename... Subtypes>
+void printVector(Vector<Subtypes...> &vector)
+{
+	for (const auto &value : vector)
+		std::cout << value << std::endl;
+}
 
 
 
 int main()
 {
-	std::vector<int, MyAllocator<int> > v = { 1, 2, 3, 4, 5 };
-	std::list<int, MyAllocator<int> > l;
-	l.push_back(3);
+	std::map<int, int> map1;
+	fillMapByFactorial(map1, 10);
 
-	std::list<int, MyAllocator<int> > l2;
-	l2 = std::move(l);
+	std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int>>> map2;
+	fillMapByFactorial(map2, 10);
+	printMap(map2);
 
-	std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int> > > m;
-	m[1] = 1;
+	MyVector<int> vector1;
+	fillVectorByRange(vector1, 10);
+	//printVector(vector1);
+
+//	for (const auto &value : vector1)
+//		std::cout << value << std::endl;
+
+	MyVector<int, MyAllocator<int>> vector2;
+	fillVectorByRange(vector2, 10);
 
 	return 0;
 }
---
+++ src/library/MyVector.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+
+template<class T, class Allocator = std::allocator<T>>
+class MyVector
+{
+
+public:
+	void push_back(const T& item);
+
+	bool isEmpty() const;
+	std::size_t size() const;
+
+	void clear();
+
+private:
+	std::size_t _size{ 0 };
+	std::size_t _capacity{ 0 };
+	T *_data{ nullptr };
+
+	Allocator _alloc;
+};
+
+
+
+template<class T, class Allocator>
+void MyVector<T, Allocator>::push_back(const T &item)
+{
+	int x = sizeof(T);
+	if (_size == _capacity)
+	{
+		std::size_t newCapacity = _capacity * 2 + 1;
+		T *newData = _alloc.allocate(newCapacity);
+	
+		if (_capacity > 0)
+		{
+			T *ptr = &_data[_size];
+			std::copy(_data, ptr, newData);
+		}
+	
+		std::swap(newData, _data);
+		_alloc.deallocate(newData, _capacity);
+		_capacity = newCapacity;
+	}
+	
+	T *ptr = &_data[_size];
+	::new ((void *)ptr) T(item);
+	++_size;
+}
+
+
+
+template<class T, class Allocator>
+bool MyVector<T, Allocator>::isEmpty() const
+{
+	return _size == 0;
+}
+
+
+
+template<class T, class Allocator>
+std::size_t MyVector<T, Allocator>::size() const
+{
+	return _size;
+}
+
+
+
+template<class T, class Allocator>
+void MyVector<T, Allocator>::clear()
+{
+	_size = 0;
+}
