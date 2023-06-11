762c958632d29c0f5d34454732c1affce01245e7
dilog777 <dima979837@yandex.ru>
2023-06-11 16:07:28 +0300

    Added 1000, 1293, 1785, 2012 tasks

---
+++ src/task_1000/main.cpp
@@ -0,0 +1,27 @@
+/*
+Вычислите a+b
+
+Исходные данные
+a и b
+
+Результат
+a+b
+
+Пример
+исходные данные		результат
+1 5					6
+*/
+
+#include <iostream>
+
+
+int main()
+{
+	int A, B;
+	std::cin >> A >> B;
+	
+	int result = A + B;
+	std::cout << result << std::endl;
+
+	return 0;
+}
